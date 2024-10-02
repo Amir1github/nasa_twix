3c1ab9b2ddf78c9de3d6c7c2207663ff2e0e4b09
Amir <121044080+Amir1github@users.noreply.github.com>
2024-10-02 21:24:40 +0500

    Add files via upload

---
+++ images.js
@@ -0,0 +1,29 @@
+let starsTexture ="./stars.jpg";
+let sunTexture ="./sun.jpg";
+let mercuryTexture = "./mercury.jpg";
+let venusTexture= "./venus.jpg";
+let earthTexture = "./earth.jpg";
+let marsTexture ="./mars.jpg";
+let jupiterTexture = "./jupiter.jpg";
+let saturnTexture = "./saturn.jpg";
+let saturnRingTexture ="./saturn ring.png";
+let  uranusTexture= "./uranus.jpg";
+let uranusRingTexture= "./uranus ring.png";
+let neptuneTexture= "./neptune.jpg";
+let plutoTexture = "./pluto.jpg";
+
+export {
+  starsTexture,
+  sunTexture,
+  mercuryTexture,
+  venusTexture,
+  earthTexture,
+  marsTexture,
+  jupiterTexture,
+  saturnTexture,
+  saturnRingTexture,
+  uranusRingTexture,
+  uranusTexture,
+  neptuneTexture,
+  plutoTexture,
+};
