0548d85718ef04ca238e8b6aa657f5f7cba5c875
Amir <121044080+Amir1github@users.noreply.github.com>
2024-10-06 23:36:54 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 document.getElementById('asteroids').addEventListener('click', function() {
         window.open('ast.html', '_blank');
 });
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.126.1/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 window.addEventListener('load', function() {
   // Предзагрузка всех изображений
   document.querySelectorAll('img').forEach(function(img) {
